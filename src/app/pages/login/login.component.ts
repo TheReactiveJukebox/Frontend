be74c5aeb1dbf0cc2dd746e496ade1dad54f6c08
Florian Scholz <florian.scholz@tu-dortmund.de>
2017-06-14 20:56:59 +0200

    [Login] Add Statuscode check for login/register.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppComponent } from '../../app.component';
 import { AuthService } from '../../services/auth/auth.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'about',
@@ -10,10 +11,12 @@ import { AuthService } from '../../services/auth/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-    loginData: {username?: string, password?: string, inviteKey?: string} = {};
+    loginData: {username?: string, password?: string} = {};
+    registerData: {username?: string, password?: string, inviteKey?: string} = {};
 
     constructor(public parent: AppComponent,
                 private authService: AuthService,
+                private translateService: TranslateService,
                 private router: Router) {}
 
     public ngOnInit() {
@@ -25,19 +28,27 @@ export class LoginComponent implements OnInit {
         this.authService.login(this.loginData).subscribe(() => {
             this.parent.tintBackground('#FFFFFF');
             this.router.navigate(['/player']);
-        }, error => {
-            console.log('Login failed!');
+        }, (error: Response) => {
+            if(error.status == 442) {
+                console.log('Login failed!');
+                alert(this.translateService.instant('LOGIN_PAGE.ERROR.WRONG_PW_OR_USER'));
+            }
         });
     }
 
     register(): void {
-        console.log('Register pressed!');
-        this.authService.registerUser(this.loginData).subscribe(data => {
+        this.authService.registerUser(this.registerData).subscribe(data => {
             this.parent.tintBackground('#FFFFFF');
             this.router.navigate(['/player']);
-        }, error => {
-            alert('Login failed. Please try again!');
-            console.log('Register failed!');
+        }, (error: Response) => {
+            if (error.status == 440) {
+                alert(this.translateService.instant('LOGIN_PAGE.ERROR.USERNAME_IN_USE'));
+            } else if (error.status == 441) {
+                alert(this.translateService.instant('LOGIN_PAGE.ERROR.WRONG_KEY'));
+            } else {
+                alert(this.translateService.instant('LOGIN_PAGE.ERROR.GENERAL'));
+            }
+            console.log('Register failed!', error);
         });
     }
 
