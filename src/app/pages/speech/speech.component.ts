cc8fe8194ed4fc78b0627d3250af288b662eea6d
Florian Scholz <florian.scholz@tu-dortmund.de>
2017-06-17 08:00:25 +0200

    [SpeechComponent] Add cleanup calls, when component gets destroyed.

--- src/app/pages/speech/speech.component.ts
+++ src/app/pages/speech/speech.component.ts
@@ -1,32 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {SpeechService} from '../../services/speech.service';
+import {Subject} from 'rxjs/Subject';
 
 @Component({
     selector: 'speech',
     styleUrls: [ './speech.component.scss' ],
     templateUrl: './speech.component.html'
 })
-export class SpeechComponent implements OnInit {
+export class SpeechComponent implements OnInit, OnDestroy {
 
     public detectedText: string;
-
     public listening: boolean;
+    private ngUnsubscribe: Subject<void>;
 
     constructor(public speechService: SpeechService) {
         this.detectedText = '';
     }
 
     ngOnInit(): void {
-
         // subscribe to Listening Observable. When ever the browser starts or stops listening, this will be called
-        this.speechService.isListening().subscribe((listening: boolean) => {
+        this.speechService.isListening().takeUntil(this.ngUnsubscribe).subscribe((listening: boolean) => {
             this.listening = listening;
         });
     }
 
+    ngOnDestroy(): void {
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
+
     // starts speech recognition and set it's result to detectedText
     public start(): void {
-        this.speechService.recordSpeech().subscribe((text: string) => {
+        this.speechService.recordSpeech().takeUntil(this.ngUnsubscribe).subscribe((text: string) => {
            this.detectedText = text;
         }, error => {
             console.log('Error: ', error);
