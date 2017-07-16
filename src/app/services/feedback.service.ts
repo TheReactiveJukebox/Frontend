0f7cbf9c99558c13c715b865dab8d5d5534bc573
molwam <ben.wilkes@tu-dortmund.de>
2017-07-16 12:56:07 +0200

    [Feedback service] implemented feedback models and service

--- src/app/services/feedback.service.ts
+++ src/app/services/feedback.service.ts
@@ -1,28 +1,81 @@
 /**
- * This service takes care of the playback control.
+ * This service takes care of feedback
  */
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {TrackService} from './track.service';
 import {SpecialFeedbackDialogComponent} from '../components/dialogs/special-feedback-dialog.component';
 import {MdDialog, MdDialogRef} from '@angular/material';
+import {Config} from '../config';
+import {Subscription} from 'rxjs/Subscription';
+import {TrackFeedback} from '../models/trackFeedback';
+import {RadiostationService} from './radiostation.service';
+import {AuthHttp} from './auth/auth-http';
+import {Track} from '../models/track';
 
 @Injectable()
-export class FeedbackService {
+export class FeedbackService implements OnDestroy {
 
     dialogRef: MdDialogRef<any>;
 
-    constructor(private trackService: TrackService, public dialog: MdDialog) {
 
+    private feedbackUrl = Config.serverUrl + '/api/track?id=';  // URL to web api
+    public currentTrack: Track;
+    private currentTrackFeedback;
+    private subscriptions: Subscription[];
+
+    constructor(private trackService: TrackService,
+                private radiostationService: RadiostationService,
+                private authHttp: AuthHttp,
+                public dialog: MdDialog) {
+        this.currentTrackFeedback = new TrackFeedback();
+        // subscribe to the currentTrack BehaviorSubject in trackService. If it get's changed, it will be automatically
+        // set to our component. The Subscription returned by subscribe() is stored, to unsubscribe, when our component
+        // gets destroyed.
+        this.subscriptions = [];
+        this.subscriptions.push(
+            this.trackService.currentTrack.subscribe(
+                (currentTrack: Track) => {
+                    this.currentTrack = currentTrack;
+                    this.trackUpdated();
+                }
+            )
+        )
+    }
+
+    ngOnDestroy(): void {
+        // VERY IMPORTANT!!! Clean up, after this component is unused. Otherwise you will left lots of unused subscriptions,
+        // which can cause heavy laggs.
+        for (let subscription of this.subscriptions
+            ) {
+            subscription.unsubscribe();
+        }
     }
 
     public like(): void {
         console.log('CALL: Like');
-        //TODO: push like to server with current song information
+        this.currentTrackFeedback.songLiked = true;
+        if (this.currentTrackFeedback.songDisliked) {
+            //We don't know if the user dislikes the song
+            this.currentTrackFeedback.songDisliked = null;
+        }
     }
 
     public dislike(): void {
         console.log('CALL: Dislike');
-        //TODO: push dislike to server with current song information
+        this.currentTrackFeedback.songDisliked = true;
+        if (this.currentTrackFeedback.songLiked) {
+            //We don't know if the user likes the song
+            this.currentTrackFeedback.songLiked = null;
+        }
+    }
+
+    private createTrackFeedbackToCurrentTrack(): TrackFeedback {
+        let feedback = new TrackFeedback();
+        if (this.currentTrack != null) {
+            feedback.trackId = this.currentTrack.id;
+            feedback.radioId = this.radiostationService.jukebox.id;
+        }
+        return feedback;
     }
 
     public getSpecialFeedback(): void {
@@ -37,5 +90,53 @@ export class FeedbackService {
     public getTendencyFeedback(): void {
         console.log('CALL tendency Feedback');
         //TODO: open tendency dialog and process information
+    }
+
+    public sendCurrentTrackFeedback(): void {
+        if (this.isCurrentTrackFeedbackValid()) {
+            console.log('Sending current track feedback');
+
+            /*
+             this.authHttp.post(this.feedbackUrl + this.currentTrackFeedback.trackId, this.currentTrackFeedback).subscribe((data: any) => {
+             console.log('FEEDBACK RETURN DATA: ', data);
+             }, (error: Response) => {
+             if (error.status == 400) {
+             console.log('The provided feedback entry is malformed');
+             }
+             console.log('Sending feedback failed: ', error);
+             });
+             */
+        } else {
+            console.warn('Trying to send invalid feedback!')
+        }
+        this.currentTrackFeedback = new TrackFeedback;
+
+    }
+
+    private trackUpdated(): void {
+        if (this.currentTrackFeedback != null) {
+            this.sendCurrentTrackFeedback();
+        }
+        this.currentTrackFeedback = this.createTrackFeedbackToCurrentTrack();
+    }
+
+    private isCurrentTrackFeedbackValid(): boolean {
+        let a = this.currentTrackFeedback.radioId != null;
+        let b = this.currentTrackFeedback.trackId != null;
+        let c = this.currentTrackFeedback.songLiked;
+        let d = this.currentTrackFeedback.songDisliked;
+        let e = this.currentTrackFeedback.artistLiked;
+        let f = this.currentTrackFeedback.artistDisliked;
+        let g = this.currentTrackFeedback.speedLiked;
+        let h = this.currentTrackFeedback.speedDisliked;
+        let i = this.currentTrackFeedback.genreLiked;
+        let j = this.currentTrackFeedback.genreDisliked;
+        let k = this.currentTrackFeedback.dynamicsLiked;
+        let l = this.currentTrackFeedback.dynamicsDisliked;
+        let m = this.currentTrackFeedback.periodLiked;
+        let n = this.currentTrackFeedback.periodDisliked;
+        let o = this.currentTrackFeedback.moodLiked;
+        let p = this.currentTrackFeedback.moodDisliked;
+        return (a && b && (c || d || e || f || g || h || i || j || k || l || m || n || o || p));
     }
 }
