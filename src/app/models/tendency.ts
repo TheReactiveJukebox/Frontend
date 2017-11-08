e17ea40d928ccd744b775c10127f7888288b1cd4
Florian Scholz <florian.scholz@tu-dortmund.de>
2017-11-08 15:07:46 +0100

    [feedback] Wrap currentTendency into BehaviorSubject.

--- src/app/models/tendency.ts
+++ src/app/models/tendency.ts
@@ -10,5 +10,16 @@ export class Tendency {
     preferredSpeed: number;
     preferredPeriodStart: number;
     preferredPeriodEnd: number;
+
+    constructor(jsonObject?) {
+        if (jsonObject) {
+            this.id = jsonObject.id;
+            this.preferredDynamics = jsonObject.preferredDynamics;
+            this.preferredSpeed = jsonObject.preferredSpeed;
+            this.preferredPeriodStart = jsonObject.preferredPeriodStart;
+            this.preferredPeriodEnd = jsonObject.preferredPeriodEnd;
+            this.userId = jsonObject.userId;
+        }
+    }
 }
 
