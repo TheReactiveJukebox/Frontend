6b86ac09d36a727705c684b73b422636cf7cad17
Florian Scholz <florian.scholz@tu-dortmund.de>
2017-12-14 15:28:04 +0100

    [survey-service] Add api-call for /api/study.

--- src/app/services/survey.service.ts
+++ src/app/services/survey.service.ts
@@ -5,12 +5,15 @@ import {MdDialog} from '@angular/material';
 import {OpenSurveyComponent} from '../components/dialogs/open-survey/open-survey.component';
 import {TrackService} from './track.service';
 import {Track} from '../models/track';
+import {AuthHttp} from './auth/auth-http';
 
 @Injectable()
 export class SurveyService {
 
     private surveyUrl: string = 'https://docs.google.com/forms/d/e/1FAIpQLSeFrq80WF_HWSxvQaaF8xvtk9YZt5_DpZ5yMxb8-EalK_YTrQ/viewform?' +
         'usp=pp_url&entry.478674814=';
+
+    private studyApiUrl: string = Config.serverUrl + '/api/study';
 
     private playedSongsTarget: number = 20;
     private startedSongsTarget: number = 30;
@@ -19,6 +22,7 @@ export class SurveyService {
     private popupOpened: boolean = false;
 
     constructor(private authService: AuthService,
+                private authHttp: AuthHttp,
                 private trackService: TrackService,
                 private dialog: MdDialog) {
         this.trackService.currentTrack.asObservable().subscribe((track: Track) => {
@@ -29,6 +33,11 @@ export class SurveyService {
     }
 
     public openSurvey(): void {
+        this.authHttp.get(this.studyApiUrl).subscribe(() => {
+           console.log('Successfully notified backend about redirection to survey!');
+        }, error => {
+            console.error('Failed to call api/study: ', error);
+        });
         window.open(this.surveyUrl + this.authService.getUsername());
     }
 
